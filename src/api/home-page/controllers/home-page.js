db26fb2c8b6fde97ea9efed28cd1cfa58e9446e3
dengluoyi <queenlixiaoman@163.com>
2026-03-27 15:14:04 +0630

    feat: 深度查询结果并返回

--- src/api/home-page/controllers/home-page.js
+++ src/api/home-page/controllers/home-page.js
@@ -6,4 +6,55 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::home-page.home-page');
+module.exports = createCoreController('api::home-page.home-page', ({ strapi }) => ({
+  async find(ctx) {
+    // Inject statically defined deep populate for home-page
+    if (!ctx.query.populate || ctx.query.populate === 'deep') {
+      ctx.query.populate = {
+        Header: { populate: '*' },
+        Banner: {
+          populate: {
+            homeBanner: { populate: '*' }
+          }
+        },
+        Cards: {
+          populate: {
+            homeFeatureCards: { populate: '*' }
+          }
+        },
+        Intro: {
+          populate: {
+            homeIntro: { populate: '*' }
+          }
+        },
+        Amenities: {
+          populate: {
+            homeAmenitiesItems: { populate: '*' },
+            homeAmenitiesImages: { populate: '*' },
+            homeAmenitiesInfo: { populate: '*' }
+          }
+        },
+        Ins: { populate: '*' },
+        Footer: {
+          populate: {
+            footerCollegeLogo: true,
+            footerSocialsIcons: { populate: '*' },
+            footerContact: { populate: '*' },
+            footerLinkColumns: {
+              populate: {
+                item: { populate: '*' }
+              }
+            },
+            footerFundLogo: true,
+            footerCopyright: { populate: '*' },
+            footerCertifications: { populate: '*' }
+          }
+        }
+      };
+    }
+    
+    // Call the default core action
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  }
+}));
