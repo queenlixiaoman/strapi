b36acb47b61407b5c1218462dedef92c41885581
dengluoyi <queenlixiaoman@163.com>
2026-03-31 14:29:51 +0630

    fix: 发布后才能预览改成存草稿后可以预览

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,28 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   'ai-chat': {
     enabled: true,
     resolve: './src/plugins/ai-chat'
+  },
+  'content-manager': {
+    enabled: true,
+    config: {
+      preview: {
+        contentTypes: [
+          {
+            uid: 'api::home-page.home-page',
+            draft: {
+              url: 'http://localhost:7003/api/preview',
+              query: {
+                type: 'home-page',
+                secret: env('PREVIEW_SECRET', 'my-secret-key'),
+              },
+            },
+            published: {
+              url: 'http://localhost:7003/',
+            },
+          },
+        ],
+      },
+    },
   },
 });
