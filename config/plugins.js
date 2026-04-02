26b4cf9cf0e03f3234ed171a4e24c7d9332ff441
dengluoyi <queenlixiaoman@163.com>
2026-04-02 14:22:42 +0630

    fix: strapi测试

--- config/plugins.js
+++ config/plugins.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
           {
             uid: 'api::home-page.home-page',
             draft: {
-              url: 'http://localhost:7003/api/preview',
+              url: 'http://localhost:7003/preview',
               query: {
                 type: 'home-page',
                 secret: env('PREVIEW_SECRET', 'my-secret-key'),
